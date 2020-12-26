41236908c1d0eaa38b2819aef416b893082aee7e
colorlessecho <colorlessecho@protonmail.com>
2020-12-26 18:56:47 +0800

    初始可行性測試

--- resource/review.js
+++
@@ -1,8 +0,0 @@
-﻿console.trace('initialization...');
-CeL.run([ /*'.data',*/ 'application.storage.file', 'interact.DOM', 'application.locale', 'application.math'],
-	initialization
-);
-
-function initialization() {
-	console.trace('initialization done');
-}
